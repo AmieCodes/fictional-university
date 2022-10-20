64dd4d99506f56bc242c391b445365c0d00938f5
Amie Frai <amie.frain@gmail.com>
2022-10-20 14:08:44 +0100

    added pagination links and modified php on dynamic post elements

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <div class="page-banner__bg-image"
         style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"> Welcom to our blog! </h1>
+        <h1 class="page-banner__title"> Welcome to our blog! </h1>
         <div class="page-banner__intro">
             <p>Keep up the god work </p>
         </div>
@@ -16,19 +16,21 @@
   while (have_posts()) {
     the_post(); ?>
     <div class="post-item">
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 
     <div class="metabox">
-      <p>Posted by <?php echo the_author(); ?> on <?php echo the_date(); ?> in <?php echo the_category(); ?></p>
+      <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
     </div>
 
     <div class="generic-content">
       <?php the_excerpt();?>
-      <p><a href="<?php the_permalink(); ?>"> Continue Reading..</p></a>
+      <p><a class="btn btn--blue" href="<?php the_permalink(); ?>"> Continue reading...</p></a>
     </div>
 
   <?php }
+  //****** Pagination *******
+  echo paginate_links();
 ?>
 </div>
 
