1d8bb4874e3d49547703d6374845ec964699a5bb
Amie Frai <amie.frain@gmail.com>
2022-10-20 12:13:17 +0100

    framework for blog post added html & php - vid22

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
+<?php get_header(); ?>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"> Welcom to our blog! </h1>
+        <div class="page-banner__intro">
+            <p>Keep up the god work </p>
+        </div>
+    </div>
+</div>
+
+<div class ="container container--narrow page-section">
 <?php
+  while (have_posts()) {
+    the_post(); ?>
+    <div class="post-item">
+      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-get_header();
 
-get_footer();
+    <div class="metabox">
+      <p>Posted by <?php echo the_author(); ?> on <?php echo the_date(); ?> in <?php echo the_category(); ?></p>
+    </div>
 
+    <div class="generic-content">
+      <?php the_excerpt();?>
+      <p><a href="<?php the_permalink(); ?>"> Continue Reading..</p></a>
+    </div>
+
+  <?php }
 ?>
+</div>
+
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -5,10 +5,10 @@ get_header();
         the_post();  ?>
 
         <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
+          <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+          <h1 class="page-banner__title"><?php the_title(); ?></h1>
+          <div class="page-banner__intro">
           <p>DONT FORGET TO REPLACE ME LATER</p>
         </div>
       </div>
