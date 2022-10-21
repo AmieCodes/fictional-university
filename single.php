e46bc5e588f34552f93715b24ea3967cb44706ed
Amie Frai <amie.frain@gmail.com>
2022-10-21 09:55:10 +0100

    added archive.php

--- single.php
+++ single.php
@@ -16,7 +16,7 @@ get_header();
 </div>
 
 <div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link"> <!-- This is the ;back home and page title box -->
+    <div class="metabox metabox--position-up metabox--with-home-link"> <!-- 'This is the back home and post info box -->
             <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home"
                         aria-hidden="true"></i> Blog Home </a> <span
                     class="metabox__main"> Posted by <?php the_author_posts_link(); ?> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
