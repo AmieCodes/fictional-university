c1894fba48baa37d63d07f09bc972ef8f166ec95
Amie Frai <amie.frain@gmail.com>
2022-11-09 11:06:47 +0000

    bug-fixes

--- front-page.php
+++ front-page.php
@@ -20,22 +20,28 @@
             $homepageEvents = new WP_Query(['posts_per_page' => 2,
               'post_type' => 'event']);
 
-            while ($homepageEvents -> have_posts()) {
-              $homepageEvents -> the_post(); ?>
+              while ($homepageEvents->have_posts()) {
+                $homepageEvents->the_post(); ?>
                 <div class="event-summary">
-                  <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"><?php the_time('M'); ?></span>
-                    <span class="event-summary__day"><?php the_time('d'); ?></span>
-                  </a>
-                  <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p> <?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                  </div>
+                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M'); ?></span>
+                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php
+                            if (has_excerpt()) {
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            }; ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    </div>
                 </div>
-              <?php }
+            <?php }
+            wp_reset_postdata();
             ?> <!-- ***** END OF WHILE LOOP ***** -->
         <!-- ******* BLUE VIEW ALL EVENTS BUTTON ******* -->
-        <p class="t-center no-margin"><a href="<?php the_permalink(); ?>" class="btn btn--blue">View All Events</a></p>
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
       </div>
     </div>
 
--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to our blog!</h1>
-    <div class="page-banner__intro">
-      <p>Keep up with our latest news.</p>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">
+            <?php if (is_page('blog')) {
+                echo 'Welcome to our Blog!';
+            } else the_archive_title(); ?>
+        </h1>
+        <div class="page-banner__intro">
+            <p><?php the_archive_description(); ?></p>
+        </div>
     </div>
-  </div>
 </div>
 
 <div class="container container--narrow page-section">
