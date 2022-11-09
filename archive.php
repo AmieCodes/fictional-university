2bdd59034d833b42abda7a557725c158d7bd6a58
Amie Frai <amie.frain@gmail.com>
2022-11-09 14:02:05 +0000

    edited-excerpt-on-homepage

--- archive.php
+++ archive.php
@@ -6,16 +6,18 @@ get_header();
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
             <?php
-            if (is_category()) {
+            if (is_page('blog')) {
+                echo 'Welcome to our Blog!';
+            } else the_archive_title();?>
+
+            <!-- if (is_category()) {
                 single_cat_title();
             }
 
             if (is_author()) {
                 echo "Posts by"; the_author();
-            }
-             /*if (is_page('blog')) {
-                echo 'Welcome to our Blog!';
-            } else the_archive_title(); */ ?>
+            } -->
+
         </h1>
         <div class="page-banner__intro">
             <p><?php the_archive_description(); ?></p>
--- front-page.php
+++ front-page.php
@@ -61,7 +61,11 @@
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+            <p><?php if (has_excerpt()) {
+              the_excerpt();
+              } else {
+                echo wp_trim_words(get_the_content(), 18);
+              } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
           </div>
         </div>
          <?php } wp_reset_postdata(); //this resets rests WP Data after calling a  custom query.
