b9624c9a7fe5293542a133670026149f4a21e59a
Amie Frai <amie.frain@gmail.com>
2023-01-05 12:12:01 +0000

    added pagBanner fnctn to all pages

--- single-program.php
+++ single-program.php
@@ -4,16 +4,10 @@
 get_header();
 
 while (have_posts()) {
-    the_post(); ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p>Replace later!</p>
-            </div>
-        </div>
-    </div>
+    the_post();
+    pageBanner();
+    ?>
+
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
@@ -114,9 +108,11 @@ while (have_posts()) {
 
 
             wp_reset_postdata();
+
             ?> <!-- ***** END OF WHILE LOOP  Events ***** -->
         </div>
-<?php
-}
+<?php }
+
 get_footer();
+
 ?>
